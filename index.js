e9879bd7797348bd29e66ada0a439fbde1bc0b2c
hammadmlk <hammadmlk@gmail.com>
2015-04-27 21:18:31 -0700

    cleanup

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ var requestHandlers = require("./requestHandlers");
 var handle = {}     
 
 handle["/"] = requestHandlers.home;
-//handle["/home"] = requestHandlers.home;
-
-//handle["/send_payment_api"] = requestHandlers.send_payment_api;
-//handle["/sample"] = requestHandlers.sample;
-//handle["/get_transactions"] = requestHandlers.get_transactions;
-
 handle["/getData"] = requestHandlers.getData;
 handle["/getUser"] = requestHandlers.getUser;
 handle["/putData"] = requestHandlers.putData;
