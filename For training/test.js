8689ca9cdcf18a95f9d43611974a1a306ff22737
habibtariq <ht752@nyu.edu>
2015-04-27 20:23:46 -0400

    added rebuild feature

--- For training/test.js
+++ For training/test.js
@@ -1,3 +1,4 @@
 var train = require("./train");
 
 train.person("TigerWoods","http://images.askmen.com/galleries/men/tiger-woods/pictures/tiger-woods-picture-1.jpg");
+train.rebuild();
--- For training/train.js
+++ For training/train.js
@@ -14,4 +14,13 @@ function person(name, imageURL){
 	  });
 }
 
+function rebuild(){
+	unirest.get("https://lambda-face-recognition.p.mashape.com/album_rebuild?album=habibTariq&albumkey=7b6caeb1864fbdd5f09fe3899cd2d436199c3b28c843580e3f61689734d717aa")
+	 .header("X-Mashape-Key", "6Zey27MELrmshOumnm1Bxn2Zap7bp1GDomAjsnBn38rYQv5LZB")
+	 .header("Accept", "application/json")
+	 .end(function (result) {
+		console.log(result.status, result.headers, result.body);
+	 });
+}
 exports.person = person;
+exports.rebuild = rebuild;
